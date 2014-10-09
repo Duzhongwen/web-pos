ce2de5c4a458e32b3022365bb365312dd14ca70d
Duzhongwen <853076601@qq.com>
2014-10-09 11:32:48 +0800

    nodepos机商品详情页面的数据读取显示

--- 文档/node-pos机/nodepos/routes/index.js
+++ 文档/node-pos机/nodepos/routes/index.js
@@ -109,42 +109,26 @@ module.exports = function(app) {
         res.redirect('/Product_list');
     });
     app.get('/admin', function (req, res) {
-        if(!req.session.property){
-            req.session.property=[];
+        if (!req.session.property) {
+            req.session.property = [];
         }
         Shop.get(function (err, product) {
-            if(err){
-                product=[];
+            if (err) {
+                product = [];
             }
             res.render('Background/admin', {
                 products: product,
                 title: "商品信息管理"
-               // Data:data
+                // Data:data
             });
         });
     });
 
-    app.get('/add_num',function(req,res) {
-       var name=req.query.name;
-       var num=req.query.number;
-       num =parseInt(num)+1;
-       Shop.update(name,num,function (err) {
-            if (err) {
-                req.flash('error', err);
-            }
-            res.redirect('/admin');
-       });
-    });
-
-    app.get('/less_num',function(req,res) {
-        var name=req.query.name;
-        var num=req.query.number;
-        if(num<=0) {
-           num=0;
-        }else{
-           num = parseInt(num) - 1;
-        }
-        Shop.update(name,num,function (err) {
+    app.get('/add_num', function (req, res) {
+        var name = req.query.name;
+        var num = req.query.number;
+        num = parseInt(num) + 1;
+        Shop.update(name, num, function (err) {
             if (err) {
                 req.flash('error', err);
             }
@@ -152,84 +136,113 @@ module.exports = function(app) {
         });
     });
 
-   app.get('/add_product',function(req,res) {
-       res.render('Background/add_product', {
-            title:"添加商品",
-            properties:req.session.property
-       })
-   });
-   app.post('/add_product',function(req,res){
-       var name=req.body.name,
-           price=req.body.price,
-           unit=req.body.unit,
-           num=req.body.num,
-           properties=req.session.property;
-       var shop = new Shop({
-           name:name,
-           price:price,
-           unit:unit,
-           num:num
-       });
-       var added_property = {};
-       if(properties.length !=0){
-           properties.forEach(function(value){
-               added_property[value.name] = req.body[value.name];
-           });
-       }
-       if(shop.num<=0){
-           req.flash('failure',"请确认商品数目");
-           res.redirect('/add_product');
-       }else {
-           shop.save(added_property,function (err) {
-               if (err) {
-                   req.flash('error', err);
-                   res.redirect('/add_product');
-               }
-               req.flash('success', "商品保存成功");
-               res.redirect('/admin');
-           });
-       }
-       req.session.property=properties;
+    app.get('/less_num', function (req, res) {
+        var name = req.query.name;
+        var num = req.query.number;
+        if (num <= 0) {
+            num = 0;
+        } else {
+            num = parseInt(num) - 1;
+        }
+        Shop.update(name, num, function (err) {
+            if (err) {
+                req.flash('error', err);
+            }
+            res.redirect('/admin');
+        });
     });
-   app.get('/delete',function(req,res) {
-       var name=req.query.name;
-            Shop.remove(name,function(err){
-                if(err){
-                   req.flash('error', err);
-                   res.redirect('/admin');
+
+    app.get('/add_product', function (req, res) {
+        res.render('Background/add_product', {
+            title: "添加商品",
+            properties: req.session.property
+        })
+    });
+    app.post('/add_product', function (req, res) {
+        var name = req.body.name,
+            price = req.body.price,
+            unit = req.body.unit,
+            num = req.body.num,
+            properties = req.session.property;
+        var shop = new Shop({
+            name: name,
+            price: price,
+            unit: unit,
+            num: num
+        });
+        var added_property = {};
+        if (properties.length != 0) {
+            properties.forEach(function (value) {
+                added_property[value.name] = req.body[value.name];
+            });
+        }
+        if (shop.num <= 0) {
+            req.flash('failure', "请确认商品数目");
+            res.redirect('/add_product');
+        } else {
+            shop.save(added_property, function (err) {
+                if (err) {
+                    req.flash('error', err);
+                    res.redirect('/add_product');
                 }
-                req.flash('success', "商品删除成功");
+                req.flash('success', "商品保存成功");
                 res.redirect('/admin');
             });
+        }
+        req.session.property = properties;
+    });
+    app.get('/delete', function (req, res) {
+        var name = req.query.name;
+        Shop.remove(name, function (err) {
+            if (err) {
+                req.flash('error', err);
+                res.redirect('/admin');
+            }
+            req.flash('success', "商品删除成功");
             res.redirect('/admin');
-   });
-   app.get('/add_properties',function(req,res){
-       res.render('Background/add_properties', {
-           title:"添加属性"
-       })
-   });
-   app.post('/add_properties',function(req,res){
-       var name=req.body.name;
-       var value=req.body.value;
-       var properties=req.session.property;
-       var property={ name:name,
-                      value:value };
-       properties.push(property);
-       req.session.property = properties;
-       res.redirect('/add_product');
-   });
-   app.get('/delete_property',function(req,res){
-       res.render('Background/delete_property',{
-           title:"删除属性",
-           property:req.session.property
-       })
-   });
-   app.get('/delete_properties',function(req,res){
-       var property_name = req.query.names;
-       var property = req.session.property;
-       var properties = _.indexOf(property,_.findWhere(property,{'name':property_name}));
-       property.splice(properties,1);
-       req.session.property=property;
-       res.redirect('/add_product');
-   })
+        });
+        res.redirect('/admin');
+    });
+    app.get('/add_properties', function (req, res) {
+        res.render('Background/add_properties', {
+            title: "添加属性"
+        })
+    });
+    app.post('/add_properties', function (req, res) {
+        var name = req.body.name;
+        var value = req.body.value;
+        var properties = req.session.property;
+        var property = { name: name,
+            value: value };
+        properties.push(property);
+        req.session.property = properties;
+        res.redirect('/add_product');
+    });
+    app.get('/delete_property', function (req, res) {
+        res.render('Background/delete_property', {
+            title: "删除属性",
+            property: req.session.property
+        })
+    });
+    app.get('/delete_properties', function (req, res) {
+        var property_name = req.query.names;
+        var property = req.session.property;
+        var properties = _.indexOf(property, _.findWhere(property, {'name': property_name}));
+        property.splice(properties, 1);
+        req.session.property = property;
+        res.redirect('/add_product');
+    });
+    app.get('/detail_product', function (req, res) {
+        var product_name = req.query.name;
+        Shop.get(function (err, product) {
+            if (err) {
+                product = [];
+            }
+            var products = _.findWhere(product,{name:product_name});
+            res.render('Background/detail_product',{
+                title: "商品详情",
+                this_product: products
+            })
+        });
+    });
 };
